36d35d32f14983c5a0fd750ac0a4e9874fbd4021
Adwa <adwaalsh01@icloud.com>
2024-10-20 21:05:38 +0300

    Initial Commit

---
+++ cal/calApp.swift
@@ -0,0 +1,17 @@
+//
+//  calApp.swift
+//  cal
+//
+//  Created by Adwa on 17/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct calApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
